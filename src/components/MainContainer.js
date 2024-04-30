8d962b59a2b53229576b497fe24da3654d281d83
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-04-30 18:06:04 +0300

    add state variables and functions to manage the portfolio, sorting, and filtering

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,30 @@
-import React from "react";
-import StockContainer from "./StockContainer";
+import React, { useState } from "react";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
+import StockContainer from "./StockContainer";
 
 function MainContainer() {
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+  const [filterBy, setFilterBy] = useState("");
+
+  const handleSortChange = (sortType) => {
+    setSortBy(sortType);
+  };
+
+  const handleFilterChange = (filterType) => {
+    setFilterBy(filterType);
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSortChange={handleSortChange} onFilterChange={handleFilterChange} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer portfolio={portfolio} setPortfolio={setPortfolio} sortBy={sortBy} filterBy={filterBy} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} setPortfolio={setPortfolio} />
         </div>
       </div>
     </div>
