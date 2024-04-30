7d7b0f1ce2b1ed69dd5e6c5238dfc15a5aba0d35
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-04-30 18:00:32 +0300

    fetch the stock data from the JSON server and render the stocks on the page

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((data) => setStocks(data));
+  }, []);
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock key={stock.id} stock={stock} />
+      ))}
     </div>
   );
 }
