fdf1301dfa04bb1008f6cf72fc6513fbdb9a2669
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-04-30 18:07:01 +0300

    handle buying and selling stocks, as well as sorting and filtering

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ portfolio, setPortfolio, sortBy, filterBy }) {
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
@@ -10,11 +10,36 @@ function StockContainer() {
       .then((data) => setStocks(data));
   }, []);
 
+  const filteredStocks = stocks.filter((stock) =>
+    filterBy ? stock.type === filterBy : true
+  );
+
+  const sortedStocks = sortBy === "alphabetical"
+    ? [...filteredStocks].sort((a, b) => a.name.localeCompare(b.name))
+    : sortBy === "price"
+    ? [...filteredStocks].sort((a, b) => a.price - b.price)
+    : filteredStocks;
+
+  const handleBuyStock = (stock) => {
+    setPortfolio([...portfolio, stock]);
+  };
+
+  const handleSellStock = (stock) => {
+    const updatedPortfolio = portfolio.filter((item) => item.id !== stock.id);
+    setPortfolio(updatedPortfolio);
+  };
+
   return (
     <div>
       <h2>Stocks</h2>
-      {stocks.map((stock) => (
-        <Stock key={stock.id} stock={stock} />
+      {sortedStocks.map((stock) => (
+        <Stock
+          key={stock.id}
+          stock={stock}
+          onBuy={handleBuyStock}
+          onSell={handleSellStock}
+          isOwned={portfolio.some((item) => item.id === stock.id)}
+        />
       ))}
     </div>
   );
